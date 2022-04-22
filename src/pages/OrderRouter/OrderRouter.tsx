2a8827c3330584bc4a655f1077843f05474a2490
CockroachCommunist <ahmednagucev@gmail.com>
2022-04-22 20:50:27 +0300

    F-02. Tab switching is done.

--- src/pages/OrderRouter/OrderRouter.tsx
+++ src/pages/OrderRouter/OrderRouter.tsx
@@ -1,11 +1,24 @@
-import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 
 import { Models } from "../../components/organisms/Models/Models";
 import { OptionalExtras } from "../../components/organisms/Optional/OptionalExtras";
 import { Place } from "../../components/organisms/Place/Place";
 import { Total } from "../../components/organisms/Total/Total";
+import { getPoint } from "../../store/point/selectors";
 
 export const OrderRouter = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const point = useSelector(getPoint);
+
+  useEffect(() => {
+    if (location.pathname !== "/order/place" && point === "") {
+      navigate("/order/place");
+    }
+  }, []);
+
   return (
     <Routes>
       <Route path="/order/place" element={<Place />} />
